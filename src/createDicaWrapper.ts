c9eae6da2892e6f1652ddc65254d307e531da3aa
unknown <fariasmauricio11@outlook.com>
2026-06-15 19:13:09 -0300

    Update files

---
+++ src/createDicaWrapper.ts
@@ -0,0 +1,53 @@
+export function createDicaWrapper(
+  prevElem: HTMLElement,
+  fileName: HTMLElement,
+  commentText: string,
+  placeholder?: HTMLElement,
+) {
+  const wrapper = document.createElement("div") as HTMLDivElement;
+  wrapper.className = "dica-leitura";
+  const img = document.createElement("img") as HTMLImageElement;
+  img.src = "../img/ico/dica_d_outline.svg";
+  img.alt = "Dica de Leitura";
+
+  // Wrap prev + link to local file
+  if (placeholder) {
+    placeholder.parentNode?.insertBefore(wrapper, placeholder);
+  } else {
+    prevElem.parentNode?.insertBefore(wrapper, prevElem);
+  }
+
+  const a = document.createElement("a") as HTMLAnchorElement;
+  const commentTextFormmated = commentText.replace("↑", "").trim();
+  const commentTextLink = "/materiais/" + commentTextFormmated;
+  a.href = commentTextLink;
+
+  // GDOC links
+  const gDocRegex = /673|21|231|89|521/g;
+  if (commentText.match(gDocRegex)) {
+    a.removeAttribute("href");
+    a.setAttribute(
+      "data-gdoc",
+      commentText.match(gDocRegex)!.filter((e) => e.match(/\d+/))![0],
+    ); // Set code exclude texts
+  }
+
+  a.target = "_blank";
+  a.className = "content-link flex-c";
+
+  const i = document.createElement("i");
+  i.className = "material-icons";
+  i.textContent = "description";
+
+  const span = document.createElement("span") as HTMLSpanElement;
+  span.appendChild(fileName);
+  fileName.replaceWith(...fileName.childNodes); // Remove 'p' tag (unwrap)
+
+  a.append(i, span);
+
+  if (placeholder) {
+    wrapper.append(img, placeholder, a);
+  } else {
+    wrapper.append(img, prevElem, a);
+  }
+}
--- src/handleLocalFile.ts
+++ src/handleLocalFile.ts
@@ -1,3 +1,5 @@
+import { createDicaWrapper } from "./createDicaWrapper";
+
 export function handleLocalFile() {
   const supComments = document.querySelectorAll(
     "sup.file",
@@ -5,9 +7,6 @@ export function handleLocalFile() {
   const commentTexts = document.querySelectorAll(
     "dd",
   ) as NodeListOf<HTMLElement>;
-
-  if (commentTexts.length !== supComments.length)
-    throw new Error("Comments length failed.");
 
   supComments.forEach((supComment, index) => {
     const fileNameWrap = supComment.closest("P") as HTMLElement;
@@ -17,52 +16,33 @@ export function handleLocalFile() {
     const previousFileNameWrap =
       fileNameWrap.previousElementSibling as HTMLElement;
 
-    if (fileNameWrap.closest(".dica-leitura")) return; // Skip if already inside a dica-leitura wrapper
+    if (!previousFileNameWrap && fileNameWrap.closest(".dica-leitura")) return; // Skip if already inside a dica-leitura wrapper
 
-    if (previousFileNameWrap && previousFileNameWrap.tagName === "P") {
-      const wrapper = document.createElement("div") as HTMLDivElement;
-      wrapper.className = "dica-leitura";
-      const img = document.createElement("img") as HTMLImageElement;
-      img.src = "../img/ico/dica_d_outline.svg";
-      img.alt = "Dica de Leitura";
-
-      // Wrap prev + link to local file
-      previousFileNameWrap.parentNode?.insertBefore(
-        wrapper,
+    // Wrap (paragraph + file link) elements if previous element file link is 'p' tag
+    if (previousFileNameWrap.tagName === "P") {
+      createDicaWrapper(
         previousFileNameWrap,
+        fileNameWrap,
+        commentTexts[index].innerText,
       );
-
-      const a = document.createElement("a") as HTMLAnchorElement;
-      const commentText = commentTexts[index].innerText;
-      const commentTextFormmated = commentText.replace("↑", "").trim();
-      const commentTextLink = "/materiais/" + commentTextFormmated;
-      a.href = commentTextLink;
-
-      // GDOC links
-      const gDocRegex = /673|21|231|89|521/g;
-      if (commentText.match(gDocRegex)) {
-        a.removeAttribute("href");
-        a.setAttribute(
-          "data-gdoc",
-          commentText.match(gDocRegex)!.filter((e) => e.match(/\d+/))![0],
-        ); // Set code exclude texts
-      }
-
-      a.target = "_blank";
-      a.className = "content-link flex-c";
-
-      const i = document.createElement("i");
-      i.className = "material-icons";
-      i.textContent = "description";
-
-      const span = document.createElement("span") as HTMLSpanElement;
-      span.appendChild(fileNameWrap);
-      fileNameWrap.replaceWith(...fileNameWrap.childNodes); // Remove 'p' tag (unwrap)
-
-      a.append(i, span);
-
-      wrapper.append(img, previousFileNameWrap, a);
-      // supComment.remove(); // Remove sup element
     }
+
+    // if previous element file link is not 'p' tag and is 'dica-leitura' wrapper
+    if (
+      previousFileNameWrap.tagName !== "P" &&
+      previousFileNameWrap.classList.contains("dica-leitura")
+    ) {
+      const placeholder = document.createElement("p");
+      fileNameWrap.parentNode!.insertBefore(placeholder, fileNameWrap);
+
+      createDicaWrapper(
+        previousFileNameWrap,
+        fileNameWrap,
+        commentTexts[index].innerText,
+        placeholder,
+      );
+    }
+
+    supComment.remove();
   });
 }
