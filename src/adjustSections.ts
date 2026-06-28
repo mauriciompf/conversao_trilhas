ee858826f92d68f2c8d829be8a58fb979974108e
Maurício Farias <fariasmauricio11@outlook.com>
2026-06-28 19:50:38 -0300

    fix: adjust first table document (word) error and add replaceTableTags()

--- src/adjustSections.ts
+++ src/adjustSections.ts
@@ -1,23 +1,54 @@
 import { contentHost } from "./main";
+import { replaceTableTags } from "./replaceTableTags";
 
 export function adjustSections() {
   const contentTables = contentHost.querySelectorAll(
     ".content-text > table",
   ) as NodeListOf<HTMLTableElement>;
 
-  // Change table element to div element
-  [...contentTables]
-    .filter((_, index) => index > 0) // Skip 'metadata' table
-    .forEach((table) => {
-      //  tr:nth-child(2) => Get only table row content, skip title (apresentacao, etapas...)
-      const content = table.querySelector(
-        "tbody > tr:nth-child(2) > td",
+  const firstTable = contentTables[0];
+
+  // Adjust metadata table and move nested tables to the top of the content host (Document Error)
+  if (!firstTable.classList.contains("tabela-padrao")) {
+    const sectionTitleElems = firstTable.querySelectorAll(
+      "p.titulo-secao:first-child",
+    );
+
+    // Filter section titles that match specific patterns (apresentacao, etapas, unidade, unica)
+    const sectionTitle = [...sectionTitleElems].filter((sectionTitleElems) =>
+      /\b(P[ÁA]GINA:?\s?)?(APRESENTA[CÇ][ÃA]O|ETAPA\s*(1|I|2|II|3|III|4|IV)|UNIDADE\s*(1|I|2|II|3|III|4|IV)|[ÚU]NICA)\b(?![ –\-])/i.test(
+        sectionTitleElems.textContent.trim(),
+      ),
+    );
+
+    // Move nested table (metadata table) to the top of the content host
+    const nestedTable = firstTable.querySelector(
+      ".tabela-padrao",
+    ) as HTMLTableElement;
+    contentHost.prepend(nestedTable);
+
+    // Wrap section title and content in a new table structure (table > tbody > tr > td) to maintain structure consistency
+    for (let i = sectionTitle.length - 1; i >= 0; i--) {
+      const tr = sectionTitle[i].closest("tr") as HTMLElement; // Section title as table row
+
+      const content = tr?.nextElementSibling?.querySelector(
+        "td",
       ) as HTMLElement;
 
-      const section = document.createElement("div");
-      section.className = "contentWrapper";
+      const trContent = document.createElement("tr"); // Section content as table row
+      trContent.appendChild(content);
+      const table = document.createElement("table");
+      const tbody = document.createElement("tbody");
+      tbody.append(tr, trContent);
+      table.appendChild(tbody);
+      firstTable.after(table); // Insert the new table structure after the first table
 
-      while (content.firstChild) section.appendChild(content.firstChild); // Append all content into the section div
-      content.replaceWith(section); // Replace 'td' element to 'div' element - section 'contentWrapper'
-    });
+      replaceTableTags(table);
+    }
+
+    firstTable.remove();
+  }
+
+  const tables = [...contentTables].filter((_, index) => index > 0); // Filter 'metadata' table
+  tables.forEach((table) => replaceTableTags(table));
 }
