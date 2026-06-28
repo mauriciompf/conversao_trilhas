042fdc2a0e300f4a2c368914946a2b3aaf431b86
Maurício Farias <fariasmauricio11@outlook.com>
2026-06-28 20:48:09 -0300

    fix: reorder function calls and enhance metadata table with type identification

--- src/processHTML.ts
+++ src/processHTML.ts
@@ -14,15 +14,63 @@ import { saveAs } from "./saveAs";
 export async function processHTML() {
   saveAs(); // Ctrl + s => save as .zip (wrap content)
 
+  handleTitles();
   adjustComments();
+  handleTables();
   adjustSections();
   handleLocalFile();
   handleVimeoVideo();
   handleYTVideo();
   handleHyperLinks();
-  handleTables();
-  handleTitles();
+  handleAllElements();
   handleFakeTables();
   assemblyLocalFiles();
-  handleAllElements();
+
+  const contentTitles = document.querySelectorAll(
+    ".content-text > .titulo-secao",
+  ) as NodeListOf<HTMLTableElement>;
+
+  // Add 'TYPE' in metada table
+  const typeRow = document.createElement("tr") as HTMLElement;
+  const typeTitle = document.createElement("td") as HTMLElement;
+  typeTitle.textContent = "Modelo";
+  const typeName = document.createElement("td") as HTMLElement;
+  const titleSections: string[] = [];
+
+  contentTitles.forEach((title) => titleSections.push(title.innerText));
+  if (
+    /APRESENTA[CÇ][AÃ]O/i.test(titleSections[0]) &&
+    /ETAPA (4|iv)/i.test(titleSections[titleSections.length - 1])
+  ) {
+    typeName.textContent = "Apresentação + 4 etapas";
+  }
+
+  if (
+    /APRESENTA[CÇ][AÃ]O/i.test(titleSections[0]) &&
+    /ETAPA (3|iii)/i.test(titleSections[titleSections.length - 1])
+  ) {
+    typeName.textContent = "Apresentação + 3 etapas";
+  }
+
+  if (
+    /ETAPA (1|i)/i.test(titleSections[0]) &&
+    /ETAPA (4|iv)/i.test(titleSections[titleSections.length - 1])
+  ) {
+    typeName.textContent = "4 Etapas";
+  }
+
+  if (/[UÚ]NICA/i.test(titleSections[0])) {
+    typeName.textContent = "Etapa Única";
+  }
+
+  typeRow?.append(typeTitle, typeName);
+
+  document
+    .querySelectorAll("table")[0]
+    .querySelector("tbody")
+    ?.appendChild(typeRow);
+
+  contentTitles.forEach((title) => {
+    title.classList.add("titulo");
+  });
 }
