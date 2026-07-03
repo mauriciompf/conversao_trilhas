46485765c13f10763e712f729e27154bc4650327
Maurício Farias <fariasmauricio11@outlook.com>
2026-07-03 14:10:35 -0300

    feat: update regex patterns for URL and YouTube links

--- src/regexConstants.ts
+++ src/regexConstants.ts
@@ -1,7 +1,10 @@
 export const urlPattern =
   /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+export const urlPatternGlobal =
+  /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 export const vimeoPattern = /(https:\/\/vimeo.com\/[^"]*)/i;
-export const youtubePattern = /(https?:\/\/www\.youtube\.com\/watch\?[^"]*)/i;
+export const youtubePattern =
+  /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=[^&\s"]+/i;
 export const gDocPattern = /673|21|231|89|521|873/;
 export const docPattern =
   /(\.doc|\.docx|application\/msword|application\/vnd.openxmlformats-officedocument.wordprocessingml.document)$/i;
